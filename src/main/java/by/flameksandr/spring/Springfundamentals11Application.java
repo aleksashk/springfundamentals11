9f859f9f2769cab685873d4e67e485f487309501
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-29 15:04:48 +0300

    Initial commit

---
+++ src/main/java/by/flameksandr/spring/Springfundamentals11Application.java
@@ -0,0 +1,13 @@
+package by.flameksandr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springfundamentals11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springfundamentals11Application.class, args);
+    }
+
+}
---
+++ src/test/java/by/flameksandr/spring/Springfundamentals11ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springfundamentals11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
