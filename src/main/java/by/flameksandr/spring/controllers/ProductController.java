b37af8959913d8ea05e53e250bbdaf044b595b6a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-29 15:35:56 +0300

    @GetMapping("/get/{name}")

--- src/main/java/by/flameksandr/spring/controllers/ProductController.java
+++ src/main/java/by/flameksandr/spring/controllers/ProductController.java
@@ -3,10 +3,7 @@ package by.flameksandr.spring.controllers;
 import by.flameksandr.spring.entities.Product;
 import by.flameksandr.spring.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -18,5 +15,10 @@ public class ProductController {
     @PostMapping("/add")
     public void addProduct(@RequestBody Product p) {
         productRepository.save(p);
+    }
+
+    @GetMapping("/get/{name}")
+    public Product getProduct(@PathVariable String name) {
+        return productRepository.findProductByName(name);
     }
 }
